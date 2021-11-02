fb986794b64937089f39719e6a71e8229a3f07db
weihong.xu <xuweihong.cn@gmail.com>
2021-11-03 01:51:58 +0800

    feat: screenshot and ocr

--- auto_cmd/__init__.py
+++ auto_cmd/__init__.py
@@ -5,8 +5,9 @@ def _get_cmd_class():
     if sys.platform == 'darwin':
         from .mac import MacAutoCmd
         return MacAutoCmd
-    # TODO: windows
-
+    if sys.platform == 'win32':
+        from .win import WinAutoCmd
+        return WinAutoCmd
 
 AutoCmd = _get_cmd_class()
 
